30dea637fe5436978ee1e9a67eb64b373d541804
jinho <jinho0309@naver.com>
2018-01-09 16:01:07 +0900

    기본적인 Custom Toolbar 생성 및 변경방법

--- app/src/main/java/hs8/ps/MainActivity.java
+++ app/src/main/java/hs8/ps/MainActivity.java
@@ -1,13 +1,17 @@
 package hs8.ps;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar=(Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
